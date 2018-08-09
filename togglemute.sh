1dac930b6c05a3c7c6c9ed0cca992700db29223e
Jason Ertel <jertel@users.noreply.github.com>
2018-08-09 10:12:44 -0400

    Minor refactoring

--- togglemute.sh
+++ togglemute.sh
@@ -2,21 +2,17 @@
 mute=${1:0}
 showNotification=${2:0}
 
-pa=pactl
-if [ "$mute" = "1" ]; then 
-  muteString=Muted
-fi
-if [ "$mute" = "0" ]; then 
-  muteString=Unmuted
-fi
-
 pacmd list-cards | grep alsa_input | sed "s/.*#//g" | sed "s/:.*//g" > /tmp/mute
 results=$(cat /tmp/mute)
 rm -f /tmp/mute
 for id in $results; do
-  $pa set-source-mute $id $mute
+  pactl set-source-mute $id $mute
 done
 
 if [ "$showNotification" = "1" ]; then
+  muteString=Unmuted
+  if [ "$mute" = "1" ]; then 
+    muteString=Muted
+  fi
   notify-send --hint=string:x-canonical-private-synchronous: $muteString
 fi
